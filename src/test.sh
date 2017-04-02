32c5c126c080a26935ba0fcd872eff4008e0d822
FabioDubois <duboisf@student.ethz.ch>
2017-04-02 23:11:41 +0200

    scripts and gitignore

---
+++ src/test.sh
@@ -0,0 +1,4 @@
+export PYTHONPATH="${PYTHONPATH}:$HOME/3rdparty/TheanoNetCore/TheanoNetCoresrc"
+
+
+python main_test.py ../data/results/linemod_realandsynth_o3_dagnet/
---
+++ src/train.sh
@@ -0,0 +1,4 @@
+
+export PYTHONPATH="${PYTHONPATH}:$HOME/3rdparty/TheanoNetCore/TheanoNetCoresrc"
+
+python main_train_dagnet.py ../configs/linemod_ras_o3_rgbd_dagnet.ini
